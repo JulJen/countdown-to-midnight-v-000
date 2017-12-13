2aa2b7d3657d98e6861411e73ff040d4706a9d34
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-13 20:23:52 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -14,10 +14,11 @@ countdown(10)
 
 
 def countdown_with_sleep
-  while number > 0
     puts "#{number} SECOND(S)!"
-    number -= 1
     sleep(0.5)
+    number -= 1
   end
   "HAPPY NEW YEAR!"
 end
+
+countdown(10)
